fd18446c51028bcc503d51c0d89c6c993a62298e
Yunqi Shao <yunqi_shao@yahoo.com>
2018-12-16 00:19:08 +0100

    Fix hanging tramp
    Tramp hangs due to my customized bash prompt, need to customized
    the prompt pattern

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,3 +1,6 @@
 (fset 'yes-or-no-p 'y-or-n-p)
-
+;; Fix tramp for customized prompt
+(setq tramp-shell-prompt-pattern
+   "\\(?:^\\|\\)[^]#$%>
+]*#?[]#$%>].* *\\(\\[[0-9;]*[a-zA-Z] *\\)*")
 (provide 'init-misc)
