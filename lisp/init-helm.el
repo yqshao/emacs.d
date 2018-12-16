b810dd52074e6f1da20e24ade6fe890a75e1c923
Yunqi Shao <yunqi_shao@yahoo.com>
2018-12-16 01:09:47 +0100

    Add some eye candy
    1. Adjust header font of helm
    2. Chagne the default Scatch message

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -3,11 +3,17 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(add-hook 'helm-before-initialize-hook
+    (lambda () 
+      (set-face-attribute 'helm-source-header nil
+                          :height 1.0)))
 (helm-mode 1)
 (setq helm-split-window-in-side-p t)
 (setq helm-split-window-default-side 'below)
 (setq helm-ff-skip-boring-files t)
 (add-to-list 'helm-boring-file-regexp-list "\~$")
+
+
 
 (provide 'init-helm)
 
--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -3,4 +3,17 @@
 (setq tramp-shell-prompt-pattern
    "\\(?:^\\|\\)[^]#$%>
 ]*#?[]#$%>].* *\\(\\[[0-9;]*[a-zA-Z] *\\)*")
+;; Quote for scratch
+(setq-default
+ initial-scratch-message
+ (concat ";; It is not knowledge, but the act of learning, not possession but the act of\n"
+	 ";; getting there, which grants the greatest enjoyment. When I have clarified\n"
+	 ";; and exhausted a subject, then I turn away from it, in order to go into\n"
+	 ";; darkness again; the never-satisfied man is so strange if he has completed\n"
+	 ";; a structure, then it is not in order to dwell in it peacefully,but in order\n"
+	 ";; to begin another. I imagine the world conqueror must feel thus, who, after\n"
+	 ";; one kingdom is scarcely conquered, stretches out his arms for others.\n"
+	 ";;\n"
+	 ";;                             - Carl Friedrich Gauss, Letter to Bolyai, 1808.\n\n"
+	 ))
 (provide 'init-misc)
