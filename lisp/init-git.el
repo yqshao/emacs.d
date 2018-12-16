cb312bbe3a88440fa5934f637641080e62a691fb
Yunqi Shao <yunqi_shao@yahoo.com>
2018-12-16 16:57:24 +0100

    Add diff-hl

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -5,4 +5,8 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
+(require-package 'diff-hl)
+(add-hook 'after-init-hook 'global-diff-hl-mode)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+
 (provide 'init-git)
