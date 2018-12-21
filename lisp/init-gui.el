5200d3a76a510e8d51722391511e5e8987a9fac5
Yunqi Shao <yunqi.shao@kemi.uu.se>
2018-12-21 14:21:35 +0100

    Change split window behavior

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -1,3 +1,5 @@
+;;; -*- lexical-binding: t -*-
+
 ;; Fix font size
 (add-to-list 'default-frame-alist
              '(font . "DejaVu Sans Mono-10"))
@@ -18,6 +20,19 @@
 (setq-default switch-window-shortcut-style 'alphabet)
 (setq-default switch-window-timeout nil)
 (global-set-key (kbd "C-x o") 'switch-window)
+
+(defun split-window-func-with-other-buffer (split-function)
+  (lambda (&optional arg)
+    "Split this window and switch to the new window unless ARG is provided."
+    (interactive "P")
+    (funcall split-function)
+    (let ((target-window (next-window)))
+      (set-window-buffer target-window (other-buffer))
+      (unless arg
+        (select-window target-window)))))
+
+(global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer 'split-window-vertically))
+(global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer 'split-window-horizontally))
 
 ;; windmove
 (unless (memq window-system '(nt w32))
