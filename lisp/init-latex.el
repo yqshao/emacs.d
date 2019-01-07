71b0bb1e88b1575cfeeb95db642df7d6c75ac822
Yunqi Shao <yunqi.shao@kemi.uu.se>
2019-01-07 17:09:15 +0100

    Add helm-bibtex for citation

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,8 +1,12 @@
 ;; latex and pdf tools
 (require-package 'auctex)
 (require-package 'pdf-tools)
+(require-package 'helm-bibtex)
 (pdf-tools-install)
 
+
+(helm-delete-action-from-source "Insert citation" helm-source-bibtex)
+(helm-add-action-to-source "Insert citation" 'helm-bibtex-insert-citation helm-source-bibtex 0)
 ;; Use pdf-tools to open PDF files
 ;; From https://emacs.stackexchange.com/questions/
 ;; 19472/how-to-let-auctex-open-pdf-with-pdf-tools
