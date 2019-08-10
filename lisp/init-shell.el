bb80bf5b7f047397e6ce32fb27f4caefebc78dc1
Yunqi Shao <yunqi_shao@yahoo.com>
2019-08-10 21:53:15 +0200

    Update shell-pop setting, remove redundent init-elpa

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,9 +1,10 @@
 (use-package shell-pop :ensure)
+(bind-key* "C-t" 'shell-pop)
 (custom-set-variables
  '(shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
  '(shell-pop-term-shell "/bin/bash")
- '(shell-pop-universal-key "C-t")
  '(shell-pop-window-size 40)	       
  '(shell-pop-full-span nil)
+ '(shell-pop-autocd-to-working-dir nil)
  '(shell-pop-window-position "bottom"))
 (provide 'init-shell)
