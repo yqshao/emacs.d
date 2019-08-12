83444160751f3c839372e920b98c59903395daf2
Yunqi Shao <yunqi.shao@kemi.uu.se>
2019-08-12 10:19:01 +0200

    Backups in temp directory

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -4,6 +4,12 @@
 (when (eval-when-compile (version< "24.4" emacs-version))
   (add-hook 'after-init-hook 'electric-indent-mode))
 
+;; Backup folders
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 ;; Undo tree
 (use-package undo-tree
   :ensure
